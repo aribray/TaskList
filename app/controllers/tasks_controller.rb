06d043a41849f07766384e1d73712ba17f0e10d3
Ariana Bray <aribray@gmail.com>
2019-04-09 15:01:32 -0700

    Completed Wave 1 tasks and a liiiiitle bit of Wave 2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,6 +7,15 @@ class TasksController < ApplicationController
   ]
 
   def index
-    @tasks = TASKS
+    @tasks = Task.all
+  end
+
+  def show
+    task_id = params[:id].to_i
+    @task = Task.find_by(id: task_id)
+
+    if @task.nil?
+      head :not_found
+    end
   end
 end
